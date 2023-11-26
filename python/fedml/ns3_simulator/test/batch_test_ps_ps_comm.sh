e9e4df7bf39543eb4ed3d57ca82c61b9d4e46975
liuliuliu0605 <liuliuliu0605@qq.com>
2023-11-26 12:01:33 +0800

    refine ns3 test batch

--- python/fedml/ns3_simulator/test/batch_test_ps_ps_comm.sh
+++ python/fedml/ns3_simulator/test/batch_test_ps_ps_comm.sh
@@ -3,24 +3,28 @@
 pattern=pfl
 underlay=geantdistance
 ps_num=9
-topo_name=balanced_tree
-model_size=10000000
+model_size=2401488
+#topo_name=complete
 #access_link_capacity=1000000
 #core_link_capacity=1000000000
 
+topo_name_list=(complete 2d_torus ring star)
 access_link_capacity_list=(1000000 10000000 100000000 1000000000)
 core_link_capacity_list=(1000000 10000000 100000000 1000000000)
 #access_link_capacity_list=(1000000)
 #core_link_capacity_list=(1000000)
-for access_link_capacity in ${access_link_capacity_list[@]};
+for topo_name in ${topo_name_list[@]};
 do
-  for core_link_capacity in ${core_link_capacity_list[@]};
+  for access_link_capacity in ${access_link_capacity_list[@]};
   do
-    mpirun -np 9 python test_ps_ps_comm.py --pattern $pattern \
-    --underlay $underlay --ps_num $ps_num \
-    --topo_name $topo_name --model_size=$model_size \
-    --access_link_capacity $access_link_capacity \
-    --core_link_capacity $core_link_capacity
+    for core_link_capacity in ${core_link_capacity_list[@]};
+    do
+      mpirun -np 9 python test_ps_ps_comm.py --pattern $pattern \
+      --underlay $underlay --ps_num $ps_num \
+      --topo_name $topo_name --model_size=$model_size \
+      --access_link_capacity $access_link_capacity \
+      --core_link_capacity $core_link_capacity
+    done
   done
 done
 
